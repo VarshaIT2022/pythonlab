4dce6dce085ad6741b062dca5339ce349e8ecb88
VarshaIT2022 <115872364+VarshaIT2022@users.noreply.github.com>
2022-12-07 14:05:32 +0530

    Create Square root.py

---
+++ Square root.py
@@ -0,0 +1,12 @@
+#Import the math library 
+
+import math 
+ 
+#Print the square root of 36
+print("Square root of 36 is: ", math.sqrt(36)) 
+ 
+#Print the square root of 6
+print("Square root of 6 is: ",math.sqrt(6)) 
+ 
+#Print the square root of 1.5
+print("Square root of 1.5 is: ",math.sqrt(1.5)) 
